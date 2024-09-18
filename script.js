5f0ad2a7e5d3c47072f179bbcbd0974546738042
Sameer ansari <ansarit209@gmail.com>
2024-09-18 16:28:52 +0530

    Hey codebase add final step complete

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 let gridContainer = document.getElementById("div-Container")
 let button = document.querySelector("button")
+
+let randomrgb = ()=>{
+    let r = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256); 
+    return `rgb(${r} , ${g}, ${b})`
+  
+}
+
 function createGrid(size){
+  let sizeOfDiv =Math.floor(560/size)
+  console.log(sizeOfDiv)
   gridContainer.innerHTML = " "
-  for(let i = 0 ; i < size * size ; i++ ){
+  for(let i = 0 ; i < size*size ; i++ ){
   let grid = document.createElement("div");
   grid.classList.add("grid-item")
   gridContainer.appendChild(grid)
+  grid.style.width = `${sizeOfDiv}px`;
+  grid.style.height = `${sizeOfDiv}px`
+  grid.addEventListener("mouseenter", ()=>{
+    grid.style.backgroundColor = randomrgb()
+  })
 }
 }
 createGrid(6)
@@ -19,4 +35,5 @@ button.addEventListener("click", ()=>{
     createGrid(noOfSquares);
   }
 })
+
 
